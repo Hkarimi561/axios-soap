7f1079115d110a1f3cab3e209533897efe426fff
hkarimi561 <hkarimi561@gmail.com>
2019-08-09 17:38:57 +0430

    This is a new commit for what I originally planned to be amended

---
+++ index.js
@@ -0,0 +1,86 @@
+import axios from "axios";
+import { parseString  } from 'react-native-xml2js';
+
+class AxiosSoap {
+    static xmlStart='<x:Envelope xmlns:x="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tme="http://tmeappuri.org/">';
+    static xmlEnd='</x:Envelope>';
+    static xml='';
+    static header='';
+    static body='';
+
+
+
+
+    static isObject = function(a) {
+        return (!!a) && (a.constructor === Object);
+    };
+    static createObjectElement(element){
+        let header = '';
+        for (let ind in element) {
+            let data = element[ind];
+            if (this.isObject(data)){
+                header+="<"+ind+">";
+                header+=this.createObjectElement(data);
+                header+="</"+ind+">";
+            }else {
+                header+="<"+ind+">"+data+"</"+ind+">";
+            }
+        }
+        return header
+    }
+
+
+    static createBody(bodyData,bodyType='x:Body'){
+        let body = '';
+        body+="<"+bodyType+">";
+        for (let ind in bodyData) {
+            let data = bodyData[ind];
+            if (this.isObject(data)){
+                body+="<"+ind+">";
+                body+=this.createObjectElement(data);
+                body+="</"+ind+">";
+            }else {
+                body+="<"+ind+">"+data+"</"+ind+">";
+            }
+        }
+        body+="</"+bodyType+">";
+        this.body=body
+    }
+    static createHeader(headerData,headerType='x:Header'){
+        let header = '';
+        header+="<"+headerType+">";
+        for (let ind in headerData) {
+            let data = headerData[ind];
+            if (this.isObject(data)){
+                header+="<"+ind+">";
+                header+=this.createObjectElement(data);
+                header+="</"+ind+">";
+            }else {
+                header+="<"+ind+">"+data+"</"+ind+">";
+            }
+        }
+        header+="</"+headerType+">";
+        this.header=header
+    }
+
+
+    static makeRequest(url){
+        this.xml=this.xmlStart+this.header+this.body+this.xmlEnd;
+        axios.post(url,
+            this.xml,
+            {headers:
+                    {'Content-Type': 'text/xml'}
+            }).then(res=>{
+                console.log(res.data)
+            var cleanedString = res.data.replace("\ufeff", "");
+            parseString(cleanedString, (err, result) => {
+                if (err) {
+                    throw (err);
+                }
+                console.log(result);
+            });
+        }).catch(err=>{console.log(err)});
+    }
+
+}
+export default AxiosSoap
